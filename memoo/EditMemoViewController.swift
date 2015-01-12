05af6c0f3e014bb10904deded00d8cc38fc518be
yuinchirn <l.serah.yuinch@gmail.com>
2015-01-12 22:46:14 +0900

    メモ作成画面機能実装

--- memoo/EditMemoViewController.swift
+++ memoo/EditMemoViewController.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+import Realm
 
 class EditMemoViewController: UIViewController {
 
+    @IBOutlet weak var memoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    // 「×」ボタンのAction:メモリストへ戻る
+    @IBAction func backToListView(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // 「save」ボタンのAction:メモ内容を更新、メモリストへ戻る
+    @IBAction func updateMemo(sender: AnyObject) {
+        
+        // メモ内容の保存
+        let realm = RLMRealm.defaultRealm()
+        let memo = Memo()
+        memo.body = memoTextView.text
+        
+        realm.transactionWithBlock() {
+            realm.addObject(memo)
+        }
+        
+        for realmBook in Memo.allObjects() {
+            // book name:realm sample
+            println("DB中身:\((realmBook as Memo).body)")
+        }
+        
+        // メモリストへ
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func backToListView(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     /*
